122c4f36fe50b5fb14718e887544fab39a59cb1b
Tyuiu.SmirnovMN@outlook.com <Tyuiu.SmirnovMN@outlook.com>
2023-09-17 11:48:33 +0500

    Task6

---
+++ Tyuiu.SmirnovMN.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SmirnovMN.Sprint0.Task6.V0.Lib;
+
+
+namespace Tyuiu.SmirnovMN.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+
+        [TestMethod]
+        public void CheckSubstractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubstractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+        
+        [TestMethod]
+        public void CheckMultuArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+
+
+
+    }
+}
